f65c61672f981a362f1e8b3ded6d3b98a8c3bb41
Web Extension <robertjenner5@outlook.com>
2025-01-30 20:31:04 +0000

    Nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,109 +1,210 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const CalculatorApp());
+  runApp(const MyApp());
 }
 
-class CalculatorApp extends StatefulWidget {
-  const CalculatorApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  State<CalculatorApp> createState() => _CalculatorAppState();
-}
-
-class _CalculatorAppState extends State<CalculatorApp> {
-  double _loanAmount = 0;
-  double _duration = 0;
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: Text("Car Loan and Mortgage Calculator"),
-        ),
-        body: Column(
-          children: [
-            Text("Select Car Loan or Mortgage"),
-            Row(
-              children: [
-                Expanded(
-                  child: Card(
-                    child: Column(
-                      children: [
-                        Icon(
-                          Icons.car_rental,
-                          size: 80
-                        ),
-                        Text("Car Loan")
-                      ],
-                    ),
-                  )
+      debugShowCheckedModeBanner: false,
+      home: const HomeBuild(),
+    );
+  }
+}
+
+class HomeBuild extends StatefulWidget {
+  const HomeBuild({super.key});
+
+  @override
+  State<HomeBuild> createState() => HomeBuildState();
+}
+
+class HomeBuildState extends State<HomeBuild> {
+  int selectedIndex = 0;
+
+  static final List<Widget> pages = [
+    const Home(),
+    const Messages(),
+    const Profile(),
+  ];
+
+  void onItemTapped(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: pages[selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: selectedIndex,
+        onTap: onItemTapped,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: "Home"
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.message),
+              label: "Messages"
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: "Profile"
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => HomeState();
+}
+
+class HomeState extends State<Home> {
+  double _loanAmount = 0;
+  double _duration = 1;
+  int selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text("Your feed"),
+      ),
+      body: Column(
+        children: [
+          const Text("Select Car Loan or Mortgage"),
+          Row(
+            children: [
+              Expanded(
+                child: Card(
+                  child: Column(
+                    children: const [
+                      Icon(Icons.car_rental, size: 80),
+                      Text("Car Loan"),
+                    ],
+                  ),
                 ),
-                Expanded(
-                  child: Card(
-                    child: Column(
-                      children: [
-                        Icon(
-                          Icons.house,
-                          size: 80,
-                        ),
-                        Text("Mortgage")
-                      ],
-                    ),
-                  )
-                )
+              ),
+              Expanded(
+                child: Card(
+                  child: Column(
+                    children: const [
+                      Icon(Icons.house, size: 80),
+                      Text("Mortgage"),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Card(
+            child: Column(
+              children: [
+                const Text("Select Loan or Mortgage Amount"),
+                Slider(
+                  value: _loanAmount,
+                  max: 400000,
+                  divisions: 1000,
+                  thumbColor: Colors.blue,
+                  activeColor: Colors.lightBlue,
+                  onChanged: (value) {
+                    setState(() {
+                      _loanAmount = value;
+                    });
+                  },
+                ),
+                Text("You selected £ ${_loanAmount.toStringAsFixed(0)}"),
+                const Text("Select Duration"),
+                Slider(
+                  value: _duration,
+                  max: 25,
+                  min: 1,
+                  divisions: 25,
+                  thumbColor: Colors.blue,
+                  activeColor: Colors.lightBlue,
+                  onChanged: (value) {
+                    setState(() {
+                      _duration = value;
+                    });
+                  },
+                ),
+                Text("You selected ${_duration.toStringAsFixed(0)} years"),
               ],
             ),
-            Expanded(
-              flex: 2,
-              child: Card(
-                child: Column(
-                  children: [
-                    Text("Select Loan or Mortgage Amount"),
-                    Slider(
-                      value: _loanAmount,
-                      max: 400000,
-                      divisions: 1000,
-                      thumbColor: Colors.blue,
-                      activeColor: Colors.lightBlue,
-                      onChanged: (value) {
-                        setState(() {
-                          _loanAmount = value;
-                        });
-                      }
-                    ),
-                    Text("You selected £ ${_loanAmount.toStringAsFixed(0)}"),
-                    Text("Select Duration"),
-                    Slider(
-                        value: _duration,
-                        max: 25,
-                        divisions: 25,
-                        thumbColor: Colors.blue,
-                        activeColor: Colors.lightBlue,
-                        onChanged: (value) {
-                          setState(() {
-                            _duration = value;
-                          });
-                        }
-                    ),
-                    Text("You selected ${_duration.toStringAsFixed(0)} years")
-                  ],
+          ),
+          Card(
+            child: Row(
+              children: [
+                Text(
+                  "Your monthly repayment will be £${(_loanAmount * _duration).toStringAsFixed(0)}",
                 ),
-              ),
+              ],
             ),
-            Card(
-              child: Row(
-                children: [
-                  Text("Your monthly repayment will be £${(_loanAmount * _duration).toStringAsFixed(0)}")
-                ],
-              ),
-            )
-          ],
-        ),
+          ),
+        ],
       ),
+    );
+  }
+}
+
+class Messages extends StatefulWidget {
+  const Messages({super.key});
+
+  @override
+  State<Messages> createState() => MessagesState();
+}
+
+class MessagesState extends State<Messages> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text("Messages"),
+      ),
+      body: Column(
+        children: [
+          Text("Messages")
+        ],
+      )
+    );
+  }
+}
+
+class Profile extends StatefulWidget {
+  const Profile({super.key});
+
+  @override
+  State<Profile> createState() => ProfileState();
+}
+
+class ProfileState extends State<Profile> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text("Profile"),
+      ),
+        body: Column(
+          children: [
+            Text("Messages")
+          ],
+        )
     );
   }
 }
